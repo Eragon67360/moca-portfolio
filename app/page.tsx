ca83c65fed9ffa1879a15c2decc2a62ca6746372
Eragon67360 <thomas-moser@orange.fr>
2023-09-07 12:35:52 +0200

    Integrated texture in model and added animation :)

--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,29 @@
-"use client"
+"use client";
 import Image from "next/image";
 
 import React from "react";
+import { motion } from "framer-motion";
 import { Canvas } from "@react-three/fiber";
 import { Model } from "@/Laptop";
 import { OrbitControls } from "@react-three/drei";
 
 export default function Home() {
+  const fadeIn = {
+    hidden: { opacity: 0 },
+    visible: { opacity: 1 },
+  };
   return (
     <div className="flex flex-col h-screen radial-bg">
-      <p className="font-impact font-normal text-5xl text-third w-[771px] ml-28 mt-64">
+      <p className="font-impact font-normal text-5xl bg-transparent text-third w-[771px] ml-28 mt-64">
         Crafting Experiences, Shaping Futures: Your UX Design Partner
       </p>
       <div className="h-full">
-        <Canvas camera={{ fov: 64, position: [-2, 2, 0] }}>
-          <ambientLight intensity={5} />
-          <OrbitControls enableZoom={true} />
-          <Model />
-        </Canvas>
+        <motion.div className="h-full" initial="hidden" animate="visible" variants={fadeIn}>
+          <Canvas camera={{ fov: 20, position: [0, 2, 0] }}>
+            <ambientLight intensity={5} />
+            <Model rotation={[-Math.PI / 2, Math.PI, 0]} />
+          </Canvas>
+        </motion.div>
       </div>
     </div>
   );
