c0f1c4147a9c7e564ea7b6a04be4f3f88e397754
Eragon67360 <thomas-moser@orange.fr>
2023-09-11 21:39:04 +0200

    Added Logo and favicon

--- components/[locale]/navigation/navbar/Logo.tsx
+++ components/[locale]/navigation/navbar/Logo.tsx
@@ -1,10 +1,11 @@
 import React from "react";
 import Link from "next/link";
-
+import Image from "next/image";
+import moca_logo from "@/public/logo.svg"
 export const Logo = () => {
   return (
     <Link href={"/"}>
-      <div className="select-none text-4xl font-bold font-impact uppercase text-black hover:text-tekhelet">UX MOCA</div>
+      <Image src={moca_logo} alt="moca Logo"/>
     </Link>
   );
 };
