b741c56791a5874c9656dc09ddf508e9c69c586c
Eragon67360 <thomas-moser@orange.fr>
2023-09-17 21:50:10 +0200

    Updated favicon and screen size breakpoints

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,6 +7,13 @@ const config: Config = {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    screens: {
+      'md': '480px',
+      //Tablet
+      'lg': '1024px',
+      //PC
+      
+    },
     extend: {
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
