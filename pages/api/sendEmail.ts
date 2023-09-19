9dec66177d31aa43e66333580b65feb3a054f573
Eragon67360 <thomas-moser@orange.fr>
2023-09-20 01:49:53 +0200

    Removed port, secure and service

--- pages/api/sendEmail.ts
+++ pages/api/sendEmail.ts
@@ -26,10 +26,9 @@ export default async function handler(
     };
 
     const transporter = nodemailer.createTransport({
-      service: "gmail",
+      
       host: "smtp.gmail.com",
-      port: 465,
-      secure: true,
+      
       auth: {
         user: user,
         pass: password,
