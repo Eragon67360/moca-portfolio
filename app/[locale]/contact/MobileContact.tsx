0f055ac933ba2c27f1ec2e50413b2fde52eff0c3
Eragon67360 <thomas-moser@orange.fr>
2023-09-27 22:10:32 +0200

    Updated mobile contact with header

--- app/[locale]/contact/MobileContact.tsx
+++ app/[locale]/contact/MobileContact.tsx
@@ -1,11 +1,21 @@
+'use client'
 import React from "react";
 import MobileVisitCard from "@/components/[locale]/mobile/MobileVisitCard";
 import MobileContactForm from "@/components/[locale]/mobile/MobileContactForm";
+import { Search } from "@/components/[locale]/desktop/navigation/navbar/Search";
+import Image from "next/image";
+import logo from "@/public/logo.svg";
+import { Language } from "@/components/[locale]/desktop/navigation/navbar/Language";
 
 function MobileContact() {
   return (
     <>
       <div className="flex flex-col space-y-10 pb-20">
+        <div className="flex justify-between pt-4 items-center px-4">
+          <Search />
+          <Image src={logo} alt="logo" width={200}/>
+          <Language />
+        </div>
         <MobileContactForm />
         <MobileVisitCard />
       </div>
