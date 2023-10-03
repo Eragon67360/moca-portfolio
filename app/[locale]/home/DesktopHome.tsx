7ecc7bc7d8b19b660439ab6522cf9034aed6ecb0
Eragon67360 <thomas-moser@orange.fr>
2023-10-03 19:00:27 +0200

    Moved code to component and added spacing

--- app/[locale]/home/DesktopHome.tsx
+++ app/[locale]/home/DesktopHome.tsx
@@ -8,17 +8,10 @@ import logo from "@/public/logo_only.svg";
 import Link from "next/link";
 import { AiOutlineFundProjectionScreen } from "react-icons/ai";
 import DesktopApps from "@/components/[locale]/desktop/DesktopApps";
+import UXSolutions from "@/components/[locale]/desktop/UXSolutions";
 import Pricings from "@/components/[locale]/desktop/Pricings";
 
 function DesktopHome() {
-  const text = `Crafting Experiences, Shaping Futures: Your UX Design Partner`;
-
-  const sectionARef = useRef(null);
-  const sectionBRef = useRef(null);
-  const scrollToRef = (ref: any) => {
-    ref.current.scrollIntoView({ behavior: "smooth" });
-  };
-
   const letterVariants: Variants = {
     initial: { opacity: 0, y: 20 },
     animate: (custom: number) => ({
@@ -87,56 +80,7 @@ function DesktopHome() {
           </Link>
         </div>
 
-        <div className="">
-          <div className="bg-white dark:bg-blackbean flex flex-col h-full pt-40 pb-40 justify-start items-center text-center select-none  space-y-6">
-            <p className="font-bold text-5xl text-blackbean dark:text-secondary">
-              Our Tailored UX Solutions
-            </p>
-            <div className="flex justify-evenly pt-14 space-x-10 mx-24">
-              <div className="flex flex-col text-centerjustify-start items-center space-y-4 px-8">
-                <AiOutlineFundProjectionScreen size={50} />
-                <h1 className="font-semibold text-2xl">New Project</h1>
-                <p>
-                  Complete new design from zero, just tell us what you need and
-                  we will work hard to make it real.
-                </p>
-              </div>
-              <div className="flex flex-col text-centerjustify-start items-center space-y-4 px-8">
-                <AiOutlineFundProjectionScreen size={50} />
-                <h1 className="font-semibold text-2xl">Redesign</h1>
-                <p>
-                  If you wan to give a more fresh look to your website or mobile
-                  application this is what you need.
-                  <br />
-                  <br />
-                  We will make sure to give you a website that suits new
-                  tendances but still follows your brand identity
-                </p>
-              </div>
-              <div className="flex flex-col text-centerjustify-start items-center space-y-4 px-8">
-                <AiOutlineFundProjectionScreen size={50} />
-                <h1 className="font-semibold text-2xl">Follow-up</h1>
-                <p>
-                  Updates, improvements and changes of an already existing
-                  website or mobile application.
-                  <br />
-                  <br />
-                  You can ask for an especific change or subscripte to our plans
-                  to get your website always updated!
-                </p>
-              </div>
-            </div>
-
-            <Image src={logo} alt="tail" />
-
-            <div className="text-blackbean flex flex-col rounded-2xl border-4 border-falured mx-80 p-8 space-y-5 bg-linen">
-              <h1 className="text-4xl font-bold">
-                Design is not just what it looks like; design is how it works
-              </h1>
-              <h2 className="text-2xl font-semibold">Steve Jobs</h2>
-            </div>
-          </div>
-        </div>
+        <UXSolutions />
 
         <div
           id="featuredprojects"
@@ -175,18 +119,22 @@ function DesktopHome() {
         </div>
 
         <div className="pt-24 bg-secondary dark:bg-blackbean flex flex-col items-center">
-          <div className="text-5xl text-blackbean dark:text-secondary font-bold text-center">Apps, websites & more</div>
-          <DesktopApps/>
-          <div className="h-24"/>
+          <div className="text-5xl text-blackbean dark:text-secondary font-bold text-center">
+            Apps, websites & more
+          </div>
+          <DesktopApps />
+          <div className="h-24" />
         </div>
 
         <div
           id="pricings"
           className="pt-24 bg-linen flex flex-col items-center"
         >
-          <div className="text-5xl text-blackbean font-bold text-center">Plans</div>
-          <Pricings/>
-          <div className="h-24"/>
+          <div className="text-5xl text-blackbean font-bold text-center">
+            Plans
+          </div>
+          <Pricings />
+          <div className="h-24" />
         </div>
       </div>
     </>
---
+++ components/[locale]/desktop/UXSolutions.tsx
@@ -0,0 +1,63 @@
+import React from "react";
+import { AiOutlineFundProjectionScreen } from "react-icons/ai";
+import Image from "next/image";
+import logo from "@/public/logo_only.svg";
+
+const UXSolutions = () => {
+  return (
+    <>
+      <div className="">
+        <div className="bg-white dark:bg-blackbean flex flex-col h-full pt-40 pb-40 justify-start items-center text-center select-none  space-y-12">
+          <p className="font-bold text-5xl text-blackbean dark:text-secondary">
+            Our Tailored UX Solutions
+          </p>
+          <div className="flex justify-evenly pt-14 space-x-10 mx-24">
+            <div className="flex flex-col text-centerjustify-start items-center space-y-4 px-8">
+              <AiOutlineFundProjectionScreen size={50} />
+              <h1 className="font-semibold text-2xl">New Project</h1>
+              <p>
+                Complete new design from zero, just tell us what you need and we
+                will work hard to make it real.
+              </p>
+            </div>
+            <div className="flex flex-col text-centerjustify-start items-center space-y-4 px-8">
+              <AiOutlineFundProjectionScreen size={50} />
+              <h1 className="font-semibold text-2xl">Redesign</h1>
+              <p>
+                If you wan to give a more fresh look to your website or mobile
+                application this is what you need.
+                <br />
+                <br />
+                We will make sure to give you a website that suits new tendances
+                but still follows your brand identity
+              </p>
+            </div>
+            <div className="flex flex-col text-centerjustify-start items-center space-y-4 px-8">
+              <AiOutlineFundProjectionScreen size={50} />
+              <h1 className="font-semibold text-2xl">Follow-up</h1>
+              <p>
+                Updates, improvements and changes of an already existing website
+                or mobile application.
+                <br />
+                <br />
+                You can ask for an especific change or subscripte to our plans
+                to get your website always updated!
+              </p>
+            </div>
+          </div>
+
+          <Image src={logo} alt="tail" />
+
+          <div className="text-blackbean mt-12 flex flex-col rounded-2xl border-4 border-falured mx-80 p-8 space-y-5 bg-linen">
+            <h1 className="text-4xl font-bold">
+              Design is not just what it looks like; design is how it works
+            </h1>
+            <h2 className="text-2xl font-semibold">Steve Jobs</h2>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default UXSolutions;
