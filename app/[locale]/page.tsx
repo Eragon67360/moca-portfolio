4fb489a48852fdb91ff1ef60cadc30f6d1231dad
Eragon67360 <thomas-moser@orange.fr>
2023-10-04 02:05:57 +0200

    Updated page component to use Resizer

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -4,30 +4,17 @@ import DesktopHome from "./home/DesktopHome";
 import MobileHome from "./home/MobileHome";
 import PhabletHome from "./home/PhabletHome";
 import TabletHome from "./home/TabletHome";
+import Resizer from "@/components/Resizer";
 
 export default function Home() {
-  const [screenWidth, setScreenWidth] = useState<number>(0);
-
-  useEffect(() => {
-    setScreenWidth(window.innerWidth);
-
-    const handleResize = () => {
-      setScreenWidth(window.innerWidth);
-    };
-
-    window.addEventListener("resize", handleResize);
-
-    // Cleanup the listener on unmount
-    return () => {
-      window.removeEventListener("resize", handleResize);
-    };
-  }, []);
-
   return (
     <>
-      {screenWidth < 640 && <DesktopHome />}
-      {screenWidth >= 640 && screenWidth < 1024 && <DesktopHome />}
-      {screenWidth >= 1024 && <DesktopHome />}
+      <Resizer
+        MobileComponent={<MobileHome />}
+        PhabletComponent={<PhabletHome />}
+        TabletComponent={<TabletHome />}
+        DesktopComponent={<DesktopHome />}
+      />
     </>
   );
 }
