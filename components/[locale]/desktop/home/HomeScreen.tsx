0ea0b84b26d9057728c9d20d37c26bde8c4c6f67
Eragon67360 <thomas-moser@orange.fr>
2023-10-06 15:31:55 +0200

    feat(HomeScreen.tsx): add support for dynamic image carousel on the home screen
    The changes in this commit add support for a dynamic image carousel on the home screen. Four new images (photo2.png, photo3.png, photo4.jpg) were added to the carousel. The carousel allows the user to click on the image to cycle through the images. The animation for transitioning between images was also added to provide a smooth visual experience.

--- components/[locale]/desktop/home/HomeScreen.tsx
+++ components/[locale]/desktop/home/HomeScreen.tsx
@@ -1,20 +1,58 @@
-import AnimatedTextCharacter from "@/components/AnimatedText";
-import React from "react";
+import React, { useState, useRef } from "react";
 import Image from "next/image";
 import paw from "@/public/home/paw.png";
 import photo1 from "@/public/home/photo1.jpg";
+import photo2 from "@/public/home/photo2.png";
+import photo3 from "@/public/home/photo3.png";
+import photo4 from "@/public/home/photo4.jpg";
+
+import AnimatedTextCharacter from "@/components/AnimatedText";
+import { motion, AnimatePresence } from "framer-motion";
 
 const DesktopHomeScreen = () => {
+  const images = [photo1, photo2, photo3, photo4];
+  const [currentIdx, setCurrentIdx] = useState(0);
+  const prevIdx = useRef(currentIdx);
+
+  const handleClick = () => {
+    prevIdx.current = currentIdx;
+    setCurrentIdx((prevIdx) => (prevIdx + 1) % images.length);
+  };
+  const direction =
+    currentIdx - prevIdx.current >= 1 || currentIdx - prevIdx.current < -1
+      ? 1
+      : -1;
+
+  const variants = {
+    initial: () => ({ opacity: 0, x: 1000 * direction, y: -200 }),
+    enter: { opacity: 1, x: 0, y: 0 },
+    exit: () => ({ opacity: 0, x: -1000 * direction, y: -200 }),
+  };
+
   return (
     <>
-      <div className="h-[100vh] bg-linen dark:bg-falured">
-        <div className="flex flex-col h-full justify-center items-center text-center select-none text-blackbean">
+      <div className="h-[100vh] bg-linen dark:bg-falured pt-24">
+        <div className="flex flex-col h-full items-center text-center select-none text-blackbean">
           <div className="flex flex-col font-bold text-5xl text-center items-center">
             <AnimatedTextCharacter />
           </div>
 
           <div className="relative w-1/3 mt-16 border">
-            <Image src={photo1} alt="photo1" className="w-full" />
+            <AnimatePresence custom={currentIdx}>
+              <motion.div
+                key={currentIdx}
+                initial="initial"
+                animate="enter"
+                exit="exit"
+                variants={variants}
+                custom={currentIdx}
+                className="w-full cursor-pointer absolute top-0"
+                onClick={handleClick}
+                transition={{ duration: 1.2 }}
+              >
+                <Image src={images[currentIdx]} alt="displayed" />
+              </motion.div>
+            </AnimatePresence>
             <Image
               src={paw}
               alt="logo"
