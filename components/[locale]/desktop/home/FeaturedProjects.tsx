9dfa046ccc761acbfca59e21e79e0c7670cadd22
Eragon67360 <thomas-moser@orange.fr>
2023-10-06 16:20:54 +0200

    refactor(FeaturedProjects.tsx): replace duplicate placeholder images with actual project images to improve visual representation feat(FeaturedProjects.tsx): add support for displaying multiple project images dynamically by iterating over an array of images

--- components/[locale]/desktop/home/FeaturedProjects.tsx
+++ components/[locale]/desktop/home/FeaturedProjects.tsx
@@ -1,10 +1,21 @@
 import React from "react";
 import Image from "next/image";
 import logo from "@/public/logo_only.svg";
+import photo1 from "@/public/home/photo1.jpg";
+import photo2 from "@/public/home/photo2.png";
+import photo3 from "@/public/home/photo3.png";
+import photo4 from "@/public/home/photo4.jpg";
 import { motion } from "framer-motion";
 import { sectionVariants } from "@/components/motionVariants";
 
 const FeaturedProjects = () => {
+  const images = [
+    { src: photo3, alt: "placeholder1" },
+    { src: photo3, alt: "placeholder2" },
+    { src: photo3, alt: "placeholder3" },
+    { src: photo3, alt: "placeholder4" },
+  ];
+
   return (
     <>
       <div id="featuredprojects" className="bg-lightblue dark:bg-darkblue">
@@ -26,62 +37,38 @@ const FeaturedProjects = () => {
           </div>
 
           <div className="grid grid-cols-2 gap-8 justify-center">
-            <div className="bg-secondary flex flex-col w-[500px]">
-              <Image
-                src={logo}
-                alt="placeholder"
-                width={200}
-                style={{ height: "auto" }}
-              />
-              <Image
-                src={logo}
-                alt="placeholder"
-                width={200}
-                style={{ height: "auto" }}
-              />
-            </div>
-            <div className="bg-secondary flex flex-col w-[500px]">
-              <Image
-                src={logo}
-                alt="placeholder"
-                width={200}
-                style={{ height: "auto" }}
-              />
-              <Image
-                src={logo}
-                alt="placeholder"
-                width={200}
-                style={{ height: "auto" }}
-              />
-            </div>
-            <div className="bg-secondary flex flex-col w-[500px]">
-              <Image
-                src={logo}
-                alt="placeholder"
-                width={200}
-                style={{ height: "auto" }}
-              />
-              <Image
-                src={logo}
-                alt="placeholder"
-                width={200}
-                style={{ height: "auto" }}
-              />
-            </div>
-            <div className="bg-secondary flex flex-col w-[500px]">
-              <Image
-                src={logo}
-                alt="placeholder"
-                width={200}
-                style={{ height: "auto" }}
-              />
-              <Image
-                src={logo}
-                alt="placeholder"
-                width={200}
-                style={{ height: "auto" }}
-              />
-            </div>
+            {images.map((image, idx) => (
+              <div
+                key={idx}
+                className="bg-secondary flex flex-col w-[500px] relative h-[500px]"
+              >
+                {" "}
+                {/* Adjust height as needed */}
+                <Image
+                  src={image.src}
+                  alt={image.alt}
+                  width={350}
+                  style={{
+                    height: "auto",
+                    position: "absolute",
+                    top: '50px',
+                    left: '30px',
+                  }}
+                />
+                <Image
+                  src={image.src}
+                  alt={image.alt}
+                  width={350}
+                  style={{
+                    height: "auto",
+                    position: "absolute",
+                    bottom: '50px',
+                    right: '30px',
+                    zIndex: 1,
+                  }}
+                />
+              </div>
+            ))}
           </div>
 
           <div className="h-24"></div>
