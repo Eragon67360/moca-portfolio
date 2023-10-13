15ab86c6949d99ac736f7a6c0110417c1721efdf
Eragon67360 <thomas-moser@orange.fr>
2023-10-13 14:32:15 +0200

    feat(navbar): add logo and language selector to mobile navbar
    The changes in this commit add a logo and a language selector to the mobile navbar component.
    
    The logo is imported from the `logo.svg` file located in the `@/public` directory. It is displayed using the `next/image` component with a width of 127 pixels.
    
    The language selector component (`Language`) is imported from the `@/components/[locale]/desktop/navigation/navbar` directory. It is added to the navbar component using a `div` element with the `flex justify-between p-6` class.

--- components/[locale]/mobile/navigation/navbar/index.tsx
+++ components/[locale]/mobile/navigation/navbar/index.tsx
@@ -3,7 +3,9 @@ import Menu from "@/components/[locale]/mobile/menu";
 import React, { useState } from "react";
 import { CgMenuGridO } from "react-icons/cg";
 import { MdOutlineClose } from "react-icons/md";
-
+import { Language } from "@/components/[locale]/desktop/navigation/navbar/Language";
+import Image from "next/image";
+import logo from "@/public/logo.svg";
 const MobileNavbar = () => {
   const [menuOpen, setMenuOpen] = useState<boolean>(false);
   const [isVisible, setIsVisible] = useState<boolean>(false);
@@ -23,6 +25,10 @@ const MobileNavbar = () => {
       {isVisible && (
         <Menu isOpen={menuOpen} onClose={() => setMenuOpen(false)} />
       )}
+      <div className="flex justify-between p-6">
+        <Image src={logo} width={127} alt="logo" />
+        <Language />
+      </div>
       <button
         aria-label="menu"
         className="fixed bottom-5 left-1/2 transform -translate-x-1/2 bg-secondary text-falured py-2 px-2 rounded-full shadow-lg hover:text-opacity-75 z-40"
