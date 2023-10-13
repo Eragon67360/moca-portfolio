07b0d7460a6f676d7e83b768a27bd296e949471a
Eragon67360 <thomas-moser@orange.fr>
2023-10-13 14:26:30 +0200

    feat(PhabletTeam.tsx): add support for fetching and displaying team members in a carousel feat(TabletTeam.tsx): add support for fetching and displaying team members in a carousel feat(MembersSkeleton.tsx): add skeleton components for loading state of team members carousel
    The changes were made to enhance the About page by adding the ability to fetch and display team members in a carousel. The PhabletTeam and TabletTeam components were modified to include the logic for fetching and rendering the team members. The MemberTemplate component was created to define the layout of each member in the carousel. Additionally, skeleton components were added to provide a loading state while the team members are being fetched.

--- app/[locale]/about/TabletTeam.tsx
+++ app/[locale]/about/TabletTeam.tsx
@@ -1,9 +1,94 @@
-import React from 'react'
+"use client";
+import React, { useEffect, useState } from "react";
+import { Carousel } from "primereact/carousel";
+import { MemberService } from "./service/MemberService";
+import Image from "next/image";
+import { MembersTabletSkeleton } from "./skeletons/MembersSkeleton";
 
-const TabletTeam = () => {
-  return (
-    <div>TabletTeam</div>
-  )
+interface Members {
+  id: string;
+  name: string;
+  role: string;
+  photo: string;
+  linkedin: string;
+  portfolio: string;
 }
 
-export default TabletTeam
+const MemberTemplate = ({
+  name,
+  role,
+  photo,
+  linkedin,
+  portfolio,
+}: Members) => {
+  return (
+    <>
+      <div className="flex flex-col justify-center items-center">
+        <Image
+          src={photo}
+          alt="image"
+          width={286}
+          height={286}
+          className="rounded-xl"
+        />
+        <div className="flex flex-col pt-9 justify-between">
+          <div className="flex flex-col">
+            <h2 className="font-semibold text-2xl">{name}</h2>
+            <h2 className="">{role}</h2>
+          </div>
+          <div className="flex space-x-3 pt-20">
+            <a
+              href={linkedin}
+              target="_blank"
+              className="rounded-full font-bold border border-cinnabar text-cinnabar hover:text-secondary hover:bg-cinnabar uppercase px-3 py-1 cursor-pointer"
+            >
+              Linkedin
+            </a>
+
+            <a
+              href={portfolio}
+              target="_blank"
+              className="rounded-full font-bold border border-cinnabar text-cinnabar hover:text-secondary hover:bg-cinnabar uppercase px-3 py-1 cursor-pointer"
+            >
+              Portfolio
+            </a>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+function TabletTeam() {
+  const [members, setMembers] = useState<Members[] | null>(null);
+
+  useEffect(() => {
+    MemberService.getMembers().then((data) => setMembers(data));
+  }, []);
+
+  return (
+    <>
+      <div className="h-screen flex flex-col justify-center items-center mx-32">
+        <div className="w-full flex flex-col justify-center items-center rounded-3xl p-8 bg-linen select-none">
+          <p className="font-bold text-5xl text-center mb-4 ">Our team</p>
+          <div className="w-full h-full">
+            {members ? (
+              <Carousel
+                value={members}
+                unstyled={false}
+                showIndicators={true}
+                numVisible={1}
+                numScroll={1}
+                itemTemplate={MemberTemplate}
+              />
+            ) : (
+              <MembersTabletSkeleton />
+            )}
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default TabletTeam;
--- app/[locale]/about/skeletons/MembersSkeleton.tsx
+++ app/[locale]/about/skeletons/MembersSkeleton.tsx
@@ -32,6 +32,65 @@ export const MembersDesktopSkeleton = () => {
   );
 };
 
+export const MembersTabletSkeleton = () => {
+  return (
+    <div className="flex flex-col justify-center items-center">
+      <SkeletonTheme baseColor="#cccccc" highlightColor="#ffffff">
+        <div className="rounded-3xl w-[230px] h-[230px]">
+          <Skeleton className="w-[230px] h-[230px]" />
+        </div>
+        <div className="flex flex-col pt-9">
+          <div className="flex flex-col">
+            <div className="h-6 w-40  mb-2 rounded">
+              <Skeleton />
+            </div>
+            <div className="h-4 w-32 rounded">
+              <Skeleton />
+            </div>
+          </div>
+          <div className="flex space-x-3 pt-20">
+            <div className="h-6 w-24 rounded">
+              <Skeleton />
+            </div>
+            <div className="h-6 w-24 rounded">
+              <Skeleton />
+            </div>
+          </div>
+        </div>
+      </SkeletonTheme>
+    </div>
+  );
+};
+
+export const MembersPhabletSkeleton = () => {
+  return (
+    <div className="flex flex-col justify-center items-center">
+      <SkeletonTheme baseColor="#cccccc" highlightColor="#ffffff">
+        <div className="rounded-3xl w-[230px] h-[230px]">
+          <Skeleton className="w-[230px] h-[230px]" />
+        </div>
+        <div className="flex flex-col pt-9">
+          <div className="flex flex-col">
+            <div className="h-6 w-40  mb-2 rounded">
+              <Skeleton />
+            </div>
+            <div className="h-4 w-32 rounded">
+              <Skeleton />
+            </div>
+          </div>
+          <div className="flex space-x-3 pt-8">
+            <div className="h-6 w-24 rounded">
+              <Skeleton />
+            </div>
+            <div className="h-6 w-24 rounded">
+              <Skeleton />
+            </div>
+          </div>
+        </div>
+      </SkeletonTheme>
+    </div>
+  );
+};
 
 export const MembersMobileSkeleton = () => {
   return (
@@ -62,5 +121,3 @@ export const MembersMobileSkeleton = () => {
     </div>
   );
 };
-
-
