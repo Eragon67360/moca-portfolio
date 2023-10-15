334e17c3bec57a8202526c9db9741852710d1257
Eragon67360 <thomas-moser@orange.fr>
2023-10-15 18:50:59 +0200

    chore(MobileContact.tsx): remove logo and language component from MobileContact to improve UI layout feat(MobileNavbar.tsx): add conditional rendering for logo and language component in MobileNavbar based on the current pathname to improve UI consistency

--- app/[locale]/contact/MobileContact.tsx
+++ app/[locale]/contact/MobileContact.tsx
@@ -10,10 +10,6 @@ function MobileContact() {
   return (
     <>
       <div className="flex flex-col space-y-10 bg-bg-mobile-light dark:bg-bg-mobile-dark bg-no-repeat bg-cover bg-center bg-fixed overflow-y-hidden">
-        <div className="flex justify-between pt-4 items-center px-4">
-          <Image src={logo} alt="logo" width={140} />
-          <Language />
-        </div>
         <MobileContactForm />
         <MobileVisitCard />
       </div>
--- components/[locale]/mobile/navigation/navbar/index.tsx
+++ components/[locale]/mobile/navigation/navbar/index.tsx
@@ -1,6 +1,7 @@
 "use client";
 import Menu from "@/components/[locale]/mobile/menu";
 import React, { useState } from "react";
+import { usePathname } from "next/navigation";
 import { CgMenuGridO } from "react-icons/cg";
 import { MdOutlineClose } from "react-icons/md";
 import { Language } from "@/components/[locale]/desktop/navigation/navbar/Language";
@@ -9,6 +10,7 @@ import logo from "@/public/logo.svg";
 const MobileNavbar = () => {
   const [menuOpen, setMenuOpen] = useState<boolean>(false);
   const [isVisible, setIsVisible] = useState<boolean>(false);
+  const pathname = usePathname();
 
   const toggleMenu = () => {
     if (menuOpen) {
@@ -22,20 +24,28 @@ const MobileNavbar = () => {
 
   return (
     <>
-      {isVisible && (
-        <Menu isOpen={menuOpen} onClose={() => setMenuOpen(false)} />
+      {pathname !== "/" && (
+        <>
+          {isVisible && (
+            <Menu isOpen={menuOpen} onClose={() => setMenuOpen(false)} />
+          )}
+          <div className="flex justify-between p-6">
+            <Image src={logo} width={127} alt="logo" />
+            <Language />
+          </div>
+          <button
+            aria-label="menu"
+            className="fixed bottom-5 left-1/2 transform -translate-x-1/2 bg-secondary text-falured py-2 px-2 rounded-full shadow-lg hover:text-opacity-75 z-40"
+            onClick={toggleMenu}
+          >
+            {menuOpen ? (
+              <MdOutlineClose size={32} />
+            ) : (
+              <CgMenuGridO size={32} />
+            )}
+          </button>
+        </>
       )}
-      <div className="flex justify-between p-6">
-        <Image src={logo} width={127} alt="logo" />
-        <Language />
-      </div>
-      <button
-        aria-label="menu"
-        className="fixed bottom-5 left-1/2 transform -translate-x-1/2 bg-secondary text-falured py-2 px-2 rounded-full shadow-lg hover:text-opacity-75 z-40"
-        onClick={toggleMenu}
-      >
-        {menuOpen ? <MdOutlineClose size={32} /> : <CgMenuGridO size={32} />}
-      </button>
     </>
   );
 };
