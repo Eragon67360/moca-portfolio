d5cfd16fe3b582fee5c6951d2717dc8687f12d3c
Eragon67360 <thomas-moser@orange.fr>
2023-10-16 17:07:52 +0200

    refactor(DesktopLanding.tsx): simplify and reorganize the structure of the DesktopLanding component for better readability and maintainability feat(DesktopLanding.tsx): update the styling and content of the DesktopLanding component to improve user experience and convey the brand message more effectively fix(DesktopLanding.tsx): fix the styling of the "Get started" button to match the design requirements

--- app/[locale]/landing/DesktopLanding.tsx
+++ app/[locale]/landing/DesktopLanding.tsx
@@ -7,46 +7,25 @@ function DesktopLanding() {
   return (
     <>
       <section className="h-screen w-full overflow-x-hidden flex select-none">
-        <div className="w-full h-full bg-blackbean flex flex-col">
-          <Image
-            src={logo}
-            alt="logo"
-            width={42}
-            className="absolute top-0 left-0 mt-4 ml-4"
-          />
-          <div className="flex flex-col justify-center items-center h-full w-screen text-secondary">
-            <div className="bg-transparent border border-secondary/30 hover:border-secondary/50 rounded-full px-2">
-              <Link href={"/about"}>
-                <span className="text-secondary/70">Announcing our new team.</span> <span className="font-bold">Read more →</span>
-              </Link>
-            </div>
+        <div className="w-full h-full bg-blackbean flex flex-col justify-center items-center text-secondary">
+          <h1 className="text-5xl font-bold p-2">Design, Develop, Deliver</h1>
+          <Image src={logo} alt="logo" width={114} className="mt-2"/>
+          <h2 className="text-2xl font-extrabold dark:text-white mt-4">
+            Your Digital Journey Starts Here.
+          </h2>
+          <p className="mb-6 font-light text-xl w-1/3 text-center mt-3">
+            Expert web design meets innovative development, tailored to showcase
+            your brand&apos;s essence.
+          </p>
 
-            <h1 className="box" id="box1">
-              Design, Develop, Deliver
-            </h1>
-            <h2 className="text-2xl font-extrabold dark:text-white">
-              Your Digital Journey Starts Here.
-            </h2>
-            <p className="mb-6 font-light text-xl w-1/3 text-center mt-4">
-              Expert web design meets innovative development, tailored to
-              showcase your brand&apos;s essence.
-            </p>
-
-            <div className="flex space-x-4">
-              {" "}
-              <Link
-                href="/home"
-                className="bg-cinnabar hover:bg-cinnabar/60 items-center justify-center px-5 py-3 mr-3 text-base text-center text-white rounded-lg"
-              >
-                Get started
-              </Link>
-              <Link
-                href="/contact"
-                className="inline-flex items-center justify-center px-5 py-3 text-base text-center"
-              >
-                Contact us
-              </Link>
-            </div>
+          <div className="flex mt-11">
+            {" "}
+            <Link
+              href="/home"
+              className="bg-cinnabar hover:bg-cinnabar/60 items-center justify-center px-5 py-3 mr-3 text-base text-center text-white rounded-full uppercase font-bold"
+            >
+              Get started
+            </Link>
           </div>
         </div>
       </section>
