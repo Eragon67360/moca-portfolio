ad8c315bdbe4b3e8bb11f0c3496e619c9570e029
Eragon67360 <thomas-moser@orange.fr>
2023-10-16 12:23:21 +0200

    style(globals.css): add .shadow-cards class to apply box-shadow to cards for better visual appeal
    feat(Plans.tsx):
    - Update pricing and descriptions for each plan to accurately reflect the services provided
    - Add subtitle to each plan to provide a brief overview of the plan
    - Add included section to each plan to list the features included in the plan
    - Add list of features for plans that have a list of included features
    - Update styling and layout of the plans section to improve readability and visual appeal
    - Add "Book a call" section with a button to book a call for more information
    - Add "Send us an e-mail" section
    - Add a section to list the graphics included in the Pro plan

--- components/[locale]/desktop/home/Plans.tsx
+++ components/[locale]/desktop/home/Plans.tsx
@@ -11,24 +11,40 @@ const Plans = () => {
   const planData = [
     {
       title: "Standard",
-      price: "€500/m",
+      subtitle: "Website follow-up. Get any update you want when you need.",
+      price: "895€/m",
       description: "Pause or cancel anytime",
       buttonText: "Get started",
-      button: true,
+      included: "You get a team to work on your website full-time.",
+      list1: "One request at a time",
+      list2: "Average 3-5 days delivery",
+      list3: "Easy credit-card payments",
+      list4: "Pause or cancel anytime",
+      list: true,
     },
     {
       title: "Pro",
-      price: "€800/m",
+      subtitle: "Website follow-up + Any Graphics you need.",
+      price: "1195€/m",
       description: "Pause or cancel anytime",
       buttonText: "Get started",
-      button: true,
+      included: "Get a website always updated and any graphics* you need",
+      list1: "One request at a time",
+      list2: "Average 3-5 days delivery",
+      list3: "Easy credit-card payments",
+      list4: "Pause or cancel anytime",
+      list: true,
     },
     {
       title: "Personalized",
-      price: "€~/m",
+      subtitle: "Personalize your plan based on your needs.",
+      price: "~€/m",
       description: "Pause or cancel anytime",
       callText: "Book a call to have more informations",
-      button: false,
+      buttonText: "Get started",
+      included:
+        "You decide what you need and we do it. This plan can be cheaper or more expensive depending on your demands.",
+      list: false,
     },
   ];
 
@@ -47,44 +63,89 @@ const Plans = () => {
           <div className="text-5xl text-blackbean dark:text-secondary font-bold text-center">
             Plans
           </div>
-          <div className="mt-24 flex flex-wrap gap-8 text-blackbean dark:text-secondary px-8">
+          <div className="mt-24 lg:mx-12 xl:mx-36 2xl:mx-96 justify-center grid grid-cols-4 gap-8 text-blackbean dark:text-secondary px-8">
             {planData.map((plan, index) => (
               <div
                 key={index}
                 className="bg-secondary dark:bg-blackbean flex flex-col p-12 rounded-2xl shadow-2xl dark:shadow-secondary/40"
               >
-                <h2 className="font-bold text-2xl">{plan.title}</h2>
-                <h3 className="font-bold text-5xl mt-8">{plan.price}</h3>
-                <p className="text-base">{plan.description}</p>
-                {plan.button ? (
-                  <div className="flex justify-center mt-8">
-                    <button className="w-full p-2 rounded-lg bg-blackbean dark:bg-linen text-secondary dark:text-blackbean hover:bg-linen hover:text-blackbean hover:dark:bg-falured hover:dark:text-secondary">
-                      {plan.buttonText}
-                    </button>
+                <div className="flex flex-col">
+                  <div className="flex flex-col">
+                    <h2 className="font-semibold text-2xl">{plan.title}</h2>
+                    <h3 className="mt-6">{plan.subtitle}</h3>
                   </div>
-                ) : (
-                  <h3 className="flex justify-center mt-8">{plan.callText}</h3>
-                )}
+
+                  <div className="flex flex-col">
+                    <h3 className="font-bold text-2xl mt-8">{plan.price}</h3>
+                    <p className="text-base">{plan.description}</p>
+
+                    <div className="flex justify-center mt-8">
+                      <button className="w-full p-2 rounded-lg bg-blackbean dark:bg-linen text-secondary dark:text-blackbean hover:bg-linen hover:text-blackbean hover:dark:bg-falured hover:dark:text-secondary">
+                        {plan.buttonText}
+                      </button>
+                    </div>
+                  </div>
+                </div>
+                <div className="flex flex-col">
+                  <hr className="mt-6 border-1 border-linen" />
+                  <div className="flex flex-col mt-4">
+                    <p>What&apos;s included:</p>
+                    <br />
+                    <p>{plan.included}</p>
+                    {plan.list && (
+                      <ul className="mt-4 ml-4">
+                        <li className="list-disc">{plan.list1}</li>
+                        <li className="list-disc">{plan.list2}</li>
+                        <li className="list-disc">{plan.list3}</li>
+                        <li className="list-disc">{plan.list4}</li>
+                      </ul>
+                    )}
+                  </div>
+                </div>
               </div>
             ))}
 
-            <div className="flex flex-col p-12 rounded-2xl shadow-2xl dark:shadow-secondary/40 justify-between">
-              <div className="flex flex-col space-y-4 text-blackbean dark:text-secondary">
-                <div className="flex justify-center">
-                  <BsCalendarEvent size={50} />
+            <div className="flex flex-col justify-between space-y-4">
+              <div className="p-12 border rounded-2xl shadow-cards">
+                <div className="flex flex-col space-y-4 text-blackbean dark:text-secondary">
+                  <div className="flex justify-center">
+                    <BsCalendarEvent size={50} />
+                  </div>
+
+                  <div className=" text-center">Book a call</div>
                 </div>
 
-                <div className=" text-center">Book a call</div>
+                <div className="flex justify-center mx-auto mt-8">
+                  <Link
+                    className="px-4 py-2 rounded-lg flex space-x-4 border border-blackbean bg-blackbean hover:bg-linen text-secondary hover:text-blackbean dark:text-blackbean dark:bg-linen hover:dark:text-secondary hover:dark:bg-blackbean"
+                    href={`/${locale}/booking`}
+                  >
+                    <span className="font-semibold">Book now</span>
+                    <MdOutlineOpenInNew size={24} />
+                  </Link>
+                </div>
               </div>
 
-              <div className="flex justify-center mt-8">
-                <Link
-                  className="w-full px-4 py-2 rounded-lg flex space-x-4 border border-blackbean bg-blackbean hover:bg-linen text-secondary hover:text-blackbean dark:text-blackbean dark:bg-linen hover:dark:text-secondary hover:dark:bg-blackbean"
-                  href={`/${locale}/booking`}
-                >
-                  <span className="font-semibold">Book now</span>
-                  <MdOutlineOpenInNew size={24} />
-                </Link>
+              <div className="p-12 border rounded-2xl shadow-cards">
+                Send us an e-mail
+              </div>
+              <div className="flex flex-col p-6 border rounded-2xl bg-falured text-secondary">
+                <p>*Graphics includes:</p>
+                <br />
+                <div className="flex ml-4 space-x-4">
+                  <ul>
+                    <li className="list-disc">Stationary Brochures</li>
+                    <li className="list-disc">Social media graphics</li>
+                    <li className="list-disc">E-mail graphics</li>
+                    <li className="list-disc">Business cards</li>
+                  </ul>
+                  <ul>
+                    <li className="list-disc">Packaging</li>
+                    <li className="list-disc">Trade show banners</li>
+                    <li className="list-disc">Blog graphics</li>
+                    <li className="list-disc">Ads</li>
+                  </ul>
+                </div>
               </div>
             </div>
           </div>
