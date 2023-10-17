fdd13303728fbf62011ec72687e2c1c500c16a09
Eragon67360 <thomas-moser@orange.fr>
2023-10-17 14:42:59 +0200

    feat(DesktopLanding.tsx): add animation to the landing page elements for a more engaging user experience
    - Add animation to the heading element to fade in after a delay of 4 seconds
    - Add animation to the logo image to scale and fade in after a delay of 4 seconds
    - Add animation to the subheading element to fade in after a delay of 4.5 seconds
    - Add typewriter effect to the paragraph element to gradually type out the text after a delay of 4.7 seconds
    - Add animation to the button element to fade in after a delay of 4.9 seconds
    
    chore(package.json): update next version to 13.5.5 and add typewriter-effect package as a dependency

--- app/[locale]/landing/DesktopLanding.tsx
+++ app/[locale]/landing/DesktopLanding.tsx
@@ -1,32 +1,89 @@
+"use client";
 import Link from "next/link";
-import React from "react";
+import React, { useEffect } from "react";
 import Image from "next/image";
 import logo from "@/public/logo_only.svg";
+import { motion, useAnimation } from "framer-motion";
+import Typewriter from "typewriter-effect";
 
 function DesktopLanding() {
+  const controls = useAnimation();
+
+  useEffect(() => {
+    controls.start({ scale: 1, opacity: 1 });
+  }, [controls]);
+
   return (
     <>
       <section className="h-screen w-full overflow-x-hidden flex select-none">
         <div className="w-full h-full bg-linen dark:bg-blackbean flex flex-col justify-center items-center">
-          <h1 className="text-5xl font-bold p-2">Design, Develop, Deliver</h1>
-          <Image src={logo} alt="logo" width={114} className="mt-2"/>
-          <h2 className="text-2xl font-extrabold mt-4">
-            Your Digital Journey Starts Here.
-          </h2>
-          <p className="mb-6 font-light text-xl w-1/3 text-center mt-3">
-            Expert web design meets innovative development, tailored to showcase
-            your brand&apos;s essence.
-          </p>
+          <motion.h1
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ delay: 4 }}
+            className="text-5xl font-bold p-2 opacity-0"
+          >
+            Design, Develop, Deliver
+          </motion.h1>
 
-          <div className="flex mt-11">
-            {" "}
+          {/* Image animation */}
+          <motion.div
+            initial={{ scale: 4, opacity: 0 }}
+            animate={controls}
+            transition={{ type: "linear", duration: 4 }}
+          >
+            <Image src={logo} alt="logo" width={114} className="mt-2" />
+          </motion.div>
+
+          <motion.h2
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ delay: 4.5 }}
+            className="text-2xl font-extrabold mt-4"
+          >
+            Your Digital Journey Starts Here.
+          </motion.h2>
+
+          <motion.p
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ delay: 4.7 }}
+            className="mb-6 font-light text-xl w-1/3 text-center mt-3"
+          >
+            <Typewriter
+              options={{
+                autoStart: true,
+                skipAddStyles: true,
+                loop: false,
+                delay:50,
+              }}
+              onInit={(typewriter) => {
+                typewriter
+                  .pauseFor(4900)
+                  .typeString(
+                    `Expert web design meets innovative development, tailored to showcase your brand's essence.`
+                  )
+                  .callFunction(() => {
+                    console.log("String typed out!");
+                  })
+                  .start();
+              }}
+            />
+          </motion.p>
+
+          <motion.div
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ delay: 4.9 }}
+            className="flex mt-11"
+          >
             <Link
               href="/home"
               className="bg-cinnabar hover:bg-cinnabar/60 items-center justify-center px-5 py-3 mr-3 text-base text-center text-white rounded-full uppercase font-bold"
             >
               Get started
             </Link>
-          </div>
+          </motion.div>
         </div>
       </section>
     </>
