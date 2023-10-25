5c2aa0d60ff6c6835a19b889011c97842bbd2e45
Eragon67360 <thomas-moser@orange.fr>
2023-10-25 14:03:37 +0200

    fix(subscriptions/page.tsx): fix import statement for useRouter from "next/navigation" to "next/router" to resolve compilation error feat(subscriptions/page.tsx): add functionality to go back to previous page when back button is clicked fix(subscriptions/page.tsx): wrap back button in a button element for accessibility and better user experience fix(subscriptions/page.tsx): remove unnecessary empty div element fix(subscriptions/page.tsx): fix indentation of Dropdown component feat(Dropdown.tsx): add import statement for useState from "react" to use state in the component fix(Dropdown.tsx): remove unnecessary empty line

--- app/[locale]/subscriptions/page.tsx
+++ app/[locale]/subscriptions/page.tsx
@@ -1,21 +1,23 @@
+"use client";
 import React from "react";
+import { useRouter } from "next/navigation";
 import { TiArrowBack } from "react-icons/ti";
 import Dropdown from "@/components/[locale]/desktop/payment/Dropdown";
 
 function Subscriptions() {
+  const router = useRouter();
   return (
     <>
       <div className="bg-linen dark:bg-blackbean px-40">
         <div className="my-32 p-8 w-full flex flex-col justify-center bg-secondary dark:bg-falured rounded-2xl">
-          <div className="">
+          <button type="button" onClick={() => router.back()}>
             <TiArrowBack size={32} />
-          </div>
+          </button>
           <h1 className="text-5xl font-bold text-center mt-4">
             Subscription plans
           </h1>
           <div className="mt-4 text-2xl">
-            <Dropdown/>
-            
+            <Dropdown />
           </div>
         </div>
       </div>
