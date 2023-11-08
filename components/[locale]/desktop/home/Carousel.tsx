3cf151023c7c4cfa74b53556328d4edfd0844e52
Eragon67360 <thomas-moser@orange.fr>
2023-11-08 14:03:23 +0100

    feat(Carousel.tsx): add Carousel component to display a slideshow of project images feat(Carousel.tsx): implement framer-motion animations for entering and exiting carousel items feat(Carousel.tsx): add swipe functionality to navigate between carousel items feat(Carousel.tsx): add automatic pagination to automatically move to the next carousel item every 5 seconds feat(HomeScreen.tsx): import and use Carousel component in DesktopHomeScreen refactor(HomeScreen.tsx): remove unused imports and code related to previous image slideshow implementation refactor(HomeScreen.tsx): remove unused variables and code related to previous image slideshow implementation feat(HomeScreen.tsx): replace previous image slideshow with Carousel component feat(HomeScreen.tsx): add styling for a circular background and text overlay in the Carousel component feat(HomeScreen.tsx): add styling for featured projects section

---
+++ components/[locale]/desktop/home/Carousel.tsx
@@ -0,0 +1,94 @@
+// components/Carousel.tsx
+"use client";
+import { useState, useEffect } from "react";
+import { motion, AnimatePresence } from "framer-motion";
+
+const projectItems = [
+  { image: "/carousel/pipas.png" },
+  { image: "/carousel/portfolio.png" },
+  { image: "/carousel/ares.png" },
+  { image: "/carousel/stopviolence.png" },
+];
+
+const carouselVariants = {
+  enter: (direction: number) => {
+    return {
+      zIndex: 0,
+      x: direction > 0 ? 800 : -800,
+      opacity: 0,
+      scale: 0.2,
+    };
+  },
+  center: {
+    zIndex: 1,
+    x: 0,
+    opacity: 1,
+    scale: 1,
+  },
+  exit: (direction: number) => {
+    return {
+      zIndex: 0,
+      x: direction < 0 ? 800 : -800,
+      opacity: 0,
+      scale: 0.2,
+    };
+  },
+};
+
+const swipeConfidenceThreshold = 10000;
+const swipePower = (offset: number, velocity: number) => {
+  return Math.abs(offset) * velocity;
+};
+
+export const Carousel = () => {
+  const [[page, direction], setPage] = useState([0, 0]);
+
+  // Function to move to next project item
+  const paginate = (newDirection: number) => {
+    setPage([page + newDirection, newDirection]);
+  };
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      paginate(1);
+    }, 5000);
+
+    return () => clearInterval(interval);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [page]);
+
+  return (
+    <div className="relative h-[323px] mx-auto">
+      <AnimatePresence initial={false} custom={direction}>
+        <motion.img
+          key={page}
+          src={projectItems[page % projectItems.length].image}
+          alt={`Project ${page}`}
+          custom={direction}
+          variants={carouselVariants}
+          initial="enter"
+          animate="center"
+          exit="exit"
+          transition={{
+            x: { duration: 1 },
+            opacity: { duration: 1 },
+            scale: { duration: 1 },
+          }}
+          className="w-full h-full object-cover absolute"
+          drag="x"
+          dragConstraints={{ left: 0, right: 0 }}
+          dragElastic={1}
+          onDragEnd={(e, { offset, velocity }) => {
+            const swipe = swipePower(offset.x, velocity.x);
+
+            if (swipe < -swipeConfidenceThreshold) {
+              paginate(1);
+            } else if (swipe > swipeConfidenceThreshold) {
+              paginate(-1);
+            }
+          }}
+        />
+      </AnimatePresence>
+    </div>
+  );
+};
--- components/[locale]/desktop/home/HomeScreen.tsx
+++ components/[locale]/desktop/home/HomeScreen.tsx
@@ -1,40 +1,8 @@
-import React, { useState, useRef, useEffect } from "react";
-import Image from "next/image";
-import paw from "@/public/home/paw.png";
-import photo1 from "@/public/home/photo1.jpg";
-import photo2 from "@/public/home/photo2.png";
-import photo3 from "@/public/home/photo3.png";
-import photo4 from "@/public/home/photo4.jpg";
-
+import React from "react";
+import { Carousel } from "./Carousel";
 import AnimatedTextCharacter from "@/components/AnimatedText";
-import { motion, AnimatePresence } from "framer-motion";
 
 const DesktopHomeScreen = () => {
-  const images = [photo4, photo4, photo4, photo4];
-  const [currentIdx, setCurrentIdx] = useState(0);
-  const prevIdx = useRef(currentIdx);
-
-  useEffect(() => {
-    const interval = setInterval(() => {
-      prevIdx.current = currentIdx;
-      setCurrentIdx((prevIdx) => (prevIdx + 1) % images.length);
-    }, 3000);
-
-    return () => {
-      clearInterval(interval);
-    };
-  }, [currentIdx, images.length]);
-
-  const direction =
-    currentIdx - prevIdx.current >= 1 || currentIdx - prevIdx.current < -1
-      ? 1
-      : -1;
-
-  const variants = {
-    initial: () => ({ opacity: 0, x: 1000 * direction, y: -200, scale: 0.2 }),
-    enter: { opacity: 1, x: 0, y: 0, scale: 1 },
-    exit: () => ({ opacity: 0, x: -1000 * direction, y: -200, scale: 0.2 }),
-  };
 
   return (
     <>
@@ -45,25 +13,8 @@ const DesktopHomeScreen = () => {
           </div>
 
           <div className="relative w-1/3 mt-16">
-            <AnimatePresence custom={currentIdx}>
-              <motion.div
-                key={currentIdx}
-                initial="initial"
-                animate="enter"
-                exit="exit"
-                variants={variants}
-                custom={currentIdx}
-                className="w-full absolute top-0"
-                transition={{ duration: 2.5 }}
-              >
-                <Image
-                  src={images[currentIdx]}
-                  priority={true}
-                  alt="displayed"
-                  width={550}
-                />
-              </motion.div>
-            </AnimatePresence>
+            <Carousel />
+
             <div className="absolute -top-10 -left-14 z-10 w-24 h-24 rounded-full bg-cinnabar" />
             <span className="absolute -top-4 -left-11 z-20 text-white font-bold transform -rotate-[18deg]">
               Featured <br /> projects
