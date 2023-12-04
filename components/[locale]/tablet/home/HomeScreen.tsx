2a777bea848d6913b2a5899468900edd9e7d91d5
Eragon67360 <thomas-moser@orange.fr>
2023-12-04 14:41:03 +0100

    Added image transition with fade effect

--- components/[locale]/tablet/home/HomeScreen.tsx
+++ components/[locale]/tablet/home/HomeScreen.tsx
@@ -9,13 +9,68 @@ import panda3 from "@/public/tablets/panda3.jpg";
 import { useTranslations } from "next-intl";
 import Arrow from "@/components/SVG/little_arrow_1";
 import { useTheme } from "next-themes";
-import { AnimatePresence, animate, motion } from "framer-motion";
+import { motion } from "framer-motion";
 
 const colorPattern = ["bg-cinnabar dark:bg-falured", "bg-linen"];
+const pandaImages = [panda1, panda2, panda3];
 
-const fadeVariants = {
-  visible: { opacity: 1 },
-  hidden: { opacity: 0 },
+const Tablet = ({ initialPanda }: { initialPanda: any }) => {
+  const [currentPanda, setCurrentPanda] = useState(initialPanda);
+  const [nextPanda, setNextPanda] = useState(
+    (initialPanda + 1) % pandaImages.length
+  );
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentPanda(nextPanda);
+      setNextPanda((nextPanda + 1) % pandaImages.length);
+    }, 3000);
+
+    return () => clearInterval(interval);
+  }, [nextPanda]);
+
+  return (
+    <motion.div
+      className="relative"
+      initial={{ x: 0, scale: 1 }}
+      animate={{ scale: 1 }}
+      transition={{ duration: 1 }}
+    >
+      <div className="z-10">
+        <Image src={tablet_base} alt="base tablet" width={376}/>
+      </div>
+
+      <motion.div
+        className="absolute top-10 left-0 -z-10"
+        key={currentPanda}
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        exit={{ opacity: 0 }}
+        transition={{ duration: 2 }}
+      >
+        <Image
+          src={pandaImages[currentPanda]}
+          alt={`panda-${currentPanda}`}
+          width={370}
+        />
+      </motion.div>
+
+      <motion.div
+        className="absolute top-10 left-0 -z-10"
+        key={nextPanda}
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        exit={{ opacity: 0 }}
+        transition={{ duration: 1 }}
+      >
+        <Image
+          src={pandaImages[nextPanda]}
+          alt={`panda-${nextPanda}`}
+          width={370}
+        />
+      </motion.div>
+    </motion.div>
+  );
 };
 
 const TabletHomeScreen = () => {
@@ -39,27 +94,6 @@ const TabletHomeScreen = () => {
       break;
   }
 
-  const [pandas, setPandas] = useState([panda1, panda2, panda3]);
-  const [keys, setKeys] = useState([1, 2, 3]);
-  const [isVisible, setIsVisible] = useState(true);
-
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setIsVisible(false); // First step: trigger fade out
-      setTimeout(() => {
-        setPandas((prevPandas) => [
-          prevPandas[1],
-          prevPandas[2],
-          prevPandas[0],
-        ]);
-        setKeys((prevKeys) => [prevKeys[1], prevKeys[2], prevKeys[0]]);
-        setIsVisible(true); // Second step: change image and fade in
-      }, 1000); // Adjust timeout to match the fade-out duration
-    }, 2000);
-
-    return () => clearInterval(interval);
-  }, []);
-
   return (
     <>
       <div className="h-[80vh] flex flex-col w-full justify-center items-center bg-linen dark:bg-falured">
@@ -74,24 +108,7 @@ const TabletHomeScreen = () => {
               animate={{ x: -500, scale: 0.8 }}
               transition={{ duration: 1, delay: 2 }}
             >
-              <div className="relative z-10">
-                <Image src={tablet_base} alt="base tablet" width={376} />
-              </div>
-              <AnimatePresence>
-                {isVisible && (
-                  <motion.div
-                    className="absolute top-10 left-0 z-0"
-                    key={keys[0]}
-                    initial="hidden"
-                    animate="visible"
-                    exit="hidden"
-                    variants={fadeVariants}
-                    transition={{ duration: 0.5 }}
-                  >
-                    <Image src={pandas[0]} alt="panda1" width={370} />
-                  </motion.div>
-                )}
-              </AnimatePresence>
+              <Tablet initialPanda={0} />
             </motion.div>
 
             <motion.div
@@ -100,44 +117,10 @@ const TabletHomeScreen = () => {
               animate={{ x: 500, scale: 0.8 }}
               transition={{ duration: 1, delay: 2 }}
             >
-              <div className="relative z-10">
-                <Image src={tablet_base} alt="base tablet" width={376} />
-              </div>
-              <AnimatePresence>
-                {isVisible && (
-                  <motion.div
-                    className="absolute top-10 left-0 z-0"
-                    key={keys[1]}
-                    initial="hidden"
-                    animate="visible"
-                    exit="hidden"
-                    variants={fadeVariants}
-                    transition={{ duration: 0.5 }}
-                  >
-                    <Image src={pandas[1]} alt="panda1" width={370} />
-                  </motion.div>
-                )}
-              </AnimatePresence>
+              <Tablet initialPanda={1} />
             </motion.div>
             <div className="relative">
-              <div className="relative z-10">
-                <Image src={tablet_base} alt="base tablet" width={376} />
-              </div>
-              <AnimatePresence>
-                {isVisible && (
-                  <motion.div
-                    className="absolute top-10 left-0 z-0"
-                    key={keys[2]}
-                    initial="hidden"
-                    animate="visible"
-                    exit="hidden"
-                    variants={fadeVariants}
-                    transition={{ duration: 0.5 }}
-                  >
-                    <Image src={pandas[2]} alt="panda1" width={370} />
-                  </motion.div>
-                )}
-              </AnimatePresence>
+              <Tablet initialPanda={2} />
             </div>
           </div>
         </div>
